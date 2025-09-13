1b7efe9a325120a95946187a4c68ad2e9ca7c2dc
KnackNarsa <narsa98@hotmail.com>
2025-09-12 20:35:36 -0400

    feat: add search all searchers in api.js

--- api.js
+++ api.js
@@ -1,4 +1,5 @@
 // api.js
+// NicholasNarsa3
 const express = require("express");
 const db = require("./database");
 const app = express();
@@ -6,8 +7,13 @@ app.use(express.json());
 
 db.connectDB(); // Ensure database is connected
 
+// Get all users
+app.get("/users", (req, res) => {
+    res.json(db.queryUsers());
+});
 
-// GET user by ID
+
+// Get user by ID
 app.get("/users/:id", (req, res) => {
     const users = db.queryUsers();
     const user = users.find(u => u.id === parseInt(req.params.id));
@@ -15,20 +21,20 @@ app.get("/users/:id", (req, res) => {
     res.json(user);
 });
 
-// POST create user
+// Post create user
 app.post("/users", (req, res) => {
     const user = db.insertUser(req.body);
     res.status(201).json(user);
 });
 
-// PUT update user
+// Put update user
 app.put("/users/:id", (req, res) => {
     const user = db.updateUser(parseInt(req.params.id), req.body);
     if (!user) return res.status(404).send("User not found");
     res.json(user);
 });
 
-// DELETE user
+// Delete user
 app.delete("/users/:id", (req, res) => {
     const deleted = db.deleteUser(parseInt(req.params.id));
     if (!deleted) return res.status(404).send("User not found");
