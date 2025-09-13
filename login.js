6f50287c77f662c33c769a5091972e0710f276e9
KnackNarsa <narsa98@hotmail.com>
2025-09-12 20:17:21 -0400

    Docs: add contents to login.js

---
+++ login.js
@@ -0,0 +1,26 @@
+// user-auth.js
+const db = require("./database"); // Use database module
+
+// Validate username/password format
+function validateLogin(username, password) {
+    if (!username || !password) return false;
+    return username.length >= 3 && password.length >= 6;
+}
+
+// Login using database users
+function login(username, password) {
+    const users = db.queryUsers();
+    if (!validateLogin(username, password)) return false;
+    const user = users.find(u => u.username === username && u.password === password);
+    return !!user;
+}
+
+function logout(username) {
+    console.log(`Logging out ${username}`);
+}
+
+function resetPassword(username, newPassword) {
+    const users = db.queryUsers();
+    const user = users.find(u => u.username === username);
+    if (user) user.password = newPassword;
+}
